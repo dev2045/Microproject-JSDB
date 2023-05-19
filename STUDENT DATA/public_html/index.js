a19721cafb3b3d11e130c821b70e0da7dcb5202c
Devesh <134004357+dev2045@users.noreply.github.com>
2023-05-19 17:24:37 +0530

    Add files via upload

---
+++ STUDENT DATA/public_html/index.js
@@ -0,0 +1,151 @@
+
+var jpdbBaseURL = "http://api.login2explore.com:5577";
+var jpdbIRL = "/api/irl";
+var jpdbIML = "/api/iml";
+var schoolDB = "SCHOOL-DB";
+var studentREL = "STUDENT-TABLE";
+var token = "90933170|-31949319314137146|90951010";
+
+    $("#stuId").focus();
+
+    function saveRecNo2LS(jsonObj)
+      {
+          var lvData = JSON.parse(jsonObj.data);
+          localStorage.setItem("recno", lvData.rec_no);
+      }
+
+      function getStuIdAsJsonObj() {
+        var stuId = $("#stuId").val();
+        var jsonStr = {
+          id: stuId
+        };
+        return JSON.stringify(jsonStr);
+      }
+      
+    function fillData(jsonObj)
+      {
+            saveRecNo2LS(jsonObj);
+            var record = JSON.parse(jsonObj.data).record;
+            $("#stuName").val(record.stuName);
+            $("#stuClass").val(record.stuClass);
+            $("#stuDOB").val(record.stuDOB);
+            $("#stuAddress").val(record.stuAddress);
+            $("#stuEnrollDate").val(record.stuEnrollDate);
+      }
+      
+    function resetForm() {
+        $("#stuId").val("");
+        $("#stuName").val("");
+        $("#stuClass").val("");
+        $("#stuDOB").val("");
+        $("#stuAddress").val("");
+        $("#stuEnrollDate").val("");
+        $("#stuId").prop("disabled",false);
+        $("#save").prop("disabled",true);
+        $("#change").prop("disabled",true);
+        $("#reset").prop("disabled",true);
+        $("#stuId").focus();
+      }
+
+
+function validateData() {
+        var stuIdVar = $("#stuId").val();
+        if (stuIdVar === "") {
+          alert("Student Roll-No Required Value");
+          $("#stuId").focus();
+          return "";
+        }
+        var stuNameVar = $("#stuName").val();
+        if (stuNameVar === "") {
+          alert("Student Name is Required Value");
+          $("#stuName").focus();
+          return "";
+        }
+        var stuClassVar = $("#stuClass").val();
+        if (stuClassVar === "") {
+          alert("Student Class is Required Value");
+          $("#stuClass").focus();
+          return "";
+        }
+
+        var stuDOBVar = $("#stuDOB").val();
+        if (stuDOBVar === "") {
+          alert("Student Birth-Date is Required Value");
+          $("stuDOB").focus();
+          return "";
+        }
+
+        var stuAddressVar = $("#stuAddress").val();
+        if (stuAddressVar === "") {
+          alert("Student Address is Required Value");
+          $("#stuAddress").focus();
+          return "";
+        }
+
+        var stuEnrollDateVar = $("stuEnrollDate").val();
+        if (stuEnrollDateVar === "") {
+          alert("Student Enrollment-Date is Required Value");
+          $("stuEnrollDate").focus();
+          return "";
+        }
+
+        var jsonStrObj = {
+          stuId: stuIdVar,
+          stuName: stuNameVar,
+          stuClass: stuClassVar,
+          stuDOB: stuDOBVar,
+          stuAddress: stuAddressVar,
+          stuEnrollDate: stuEnrollDateVar
+        };
+        return JSON.stringify(jsonStrObj);
+      }
+      
+      
+      function getStu() {
+        var stuIdJsonObj = getStuIdAsJsonObj();
+        var getRequest = createGET_BY_KEYRequest(token,schoolDB,studentREL,stuIdJsonObj);
+        jQuery.ajaxSetup({ async: false });
+        var resJsonObj = executeCommandAtGivenBaseUrl(getRequest,jpdbBaseURL,jpdbIRL);
+        jQuery.ajaxSetup({ async: true });
+        if (resJsonObj.status === 400) {
+          $("#save").prop("disabled", false);
+          $("#reset").prop("disabled", false);
+          $("#stuName").focus();
+        } 
+        else if (resJsonObj.status === 200) {
+          $("#stuId").prop("disabled", true);
+          fillData(resJsonObj);
+          
+          $("#change").prop("disabled", false);
+          $("#reset").prop("disabled", false);
+          $("#stuName").focus();
+        }
+      }
+
+    function saveData() {
+        var jsonStrObj = validateData();
+        if (jsonStrObj === "") {
+          return "";
+        }
+        var putRequest = createPUTRequest(token,jsonStrObj,schoolDB,studentREL);
+        alert(putRequest);
+        jQuery.ajaxSetup({ async: false });
+        var resJsonObj = executeCommandAtGivenBaseUrl(putRequest,jpdbBaseURL,jpdbIML);
+        jQuery.ajaxSetup({ async: true });
+        alert(JSON.stringify(resJsonObj));
+        resetForm();
+        $("#stuId").focus();
+      }
+      
+      
+    function changeData() {
+        $("#change").prop("disabled", true);
+        jsonChg = validateData();
+        var updateRequest = createUPDATERecordRequest(token,jsonChg,schoolDB,studentREL,localStorage.getItem("recno"));
+        jQuery.ajaxSetup({async: false});
+        var resJsonObj = executeCommandAtGivenBaseUrl(updateRequest,jpdbBaseURL,jpdbIML);
+        jQuery.ajaxSetup({async: true});
+        console.log(resJsonObj);
+        resetForm();
+        $("#stuId").focus();
+      }
